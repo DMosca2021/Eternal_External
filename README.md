8db1ffcbe5349510b5d6822d2da0ae09499b6be4
jkworks <info@jilliankayworks.com>
2021-08-03 18:52:31 -0600

    updated user pages

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # adj_project1
+
+//compare quiz answers of user 1 vs. user 2 
+//based on answers true/false certain out puts will show up 
+//store user information for later use
+//use moment to send random emails to user 1 
+// can we create a username and password to store the users information? 
+// figure out how to link the user taking the quiz and user giving the advice 
+// 2nd user getting answers share the quiz questions 
+//share info between users 
+// whatever question you ask 
+// link the quiz to the API responses 
+
+
+// group advice into different categories 
+//put certain advice into one array chosen by dead person 
+//the living would get a random piece of advice depending on their checked box from the dead array 
+
--- assets/scriptU1.js
+++ assets/scriptU1.js
@@ -1,21 +1,33 @@
 
 const user1Input = document.querySelector("#userinput1");
+const numberBg = document.getElementById("#main")
 
 
-
+numberBg.createAttribute("img",)
 
 form.addEventListener('submit', function(event) {
     event.preventDefault();
     var user1Input = document.querySelector('#userinput1').value;
     console.log("user1Input " + user1Input)
-        var tempObject = {
-            user1: user1Input, 
-            
-        }
-
-        nameStored.push(tempObject)
-        localStorage.setItem("scores",JSON.stringify(nameStored));
+    
+        localStorage.setItem("name",JSON.stringify(user1Input));
         document.querySelector('#userinput1').value = "";
-    displayHS()
     }
 );
+
+
+
+
+fetch("https://twinword-emotion-analysis-v1.p.rapidapi.com/analyze/?text=After%20living%20abroad%20for%20such%20a%20long%20time%2C%20seeing%20my%20family%20was%20the%20best%20present%20I%20could%20have%20ever%20wished%20for.", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-key": "5b7389e2b1msha3f2a9a2397902ep15e923jsnca14c8f04f77",
+		"x-rapidapi-host": "twinword-emotion-analysis-v1.p.rapidapi.com"
+	}
+})
+.then(response => {
+	console.log(response);
+})
+.catch(err => {
+	console.error(err);
+});
