95a1f34b6f2c4dd615d576e8ee961430b6349857
jkworks <info@jilliankayworks.com>
2021-08-03 22:05:05 -0600

    updated js

--- assets/scriptU1.js
+++ assets/scriptU1.js
@@ -1,8 +1,8 @@
 
 const user1Input = document.querySelector("#userinput1");
 const numberBg = document.getElementById("#main")
-const feelingInputForm = document.getElementById("#feelingInput");
-const feelingBtn = document.getElementById("#feelingBtn")
+const feelingInputForm = document.getElementById("feelingInput");
+const feelingBtn = document.querySelector("#feelingBtn")
 
 
 
@@ -21,13 +21,17 @@ form.addEventListener('submit', function(event) {
 
 // "https://twinword-emotion-analysis-v1.p.rapidapi.com/analyze/?text=After%20living%20abroad%20for%20such%20a%20long%20time%2C%20seeing%20my%20family%20was%20the%20best%20present%20I%20could%20have%20ever%20wished%20for."
 
+//instead of spaces need %20 
+//.replace(" ") %20  feeling input 
 function getFeeling () {
-
+console.log(feelingInputForm.value)
 	if (feelingInputForm !== null) {
 		feelingInput = feelingInputForm.value 
 	}
+var feelingAPI = "https://twinword-emotion-analysis-v1.p.rapidapi.com/analyze/?text=" + feelingInput
+console.log(feelingAPI)
+fetch(feelingAPI, {
 
-fetch("https://twinword-emotion-analysis-v1.p.rapidapi.com/analyze/?text=" + feelingInput, {
 	"method": "GET",
 	"headers": {
 		"x-rapidapi-key": "5b7389e2b1msha3f2a9a2397902ep15e923jsnca14c8f04f77",
@@ -53,8 +57,6 @@ return data
 });
 
 }
-
-getFeeling()
 
 
 form.addEventListener('submit', function(event) {
