542f61c16029ae9f4be5c60b75d5935809989c11
jkworks <info@jilliankayworks.com>
2021-08-04 23:43:08 -0600

    css new color u1

--- assets/scriptU1.js
+++ assets/scriptU1.js
@@ -16,9 +16,6 @@ form.addEventListener('submit', function(event) {
     }
 );
 
-
-
-
 // "https://twinword-emotion-analysis-v1.p.rapidapi.com/analyze/?text=After%20living%20abroad%20for%20such%20a%20long%20time%2C%20seeing%20my%20family%20was%20the%20best%20present%20I%20could%20have%20ever%20wished%20for."
 
 //instead of spaces need %20 
